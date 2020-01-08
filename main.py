7023dbfe452d3dbb5091e08d7f220dcb0ae6a8fc
kalortpol <58624995+kalortpol@users.noreply.github.com>
2020-01-08 21:51:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # IMPORTS
 import pygame
 pygame.init()
-import classesNYAST as c
-c.theapp.init_game()
-c.spells.print_spell_dict()
-c.spells.activate_spell("harm")
-c.spells.print_spell_dict()
+import classes3 as c
+
+engine = c.Engine()
+c.engine.start()
